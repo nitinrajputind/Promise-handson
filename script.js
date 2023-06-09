1a91e054bef7619302d2beb3fa860e84bc77517a
nitinrajputind <nitinrajput971625@gmail.com>
2023-06-09 17:57:00 +0530

    promise handson completed

--- script.js
+++ script.js
@@ -13,6 +13,11 @@ function displayDate(name, age){
 displayDate("Nitin", calculate(19,1));
 
 
+
+
+
+
+
 // Question 2 
 
 let ans2 = document.getElementById("ans2")
@@ -48,6 +53,9 @@ setTimeout(()=>{
 
 
 
+
+
+
 // Question 3 
 let ans3 = document.getElementById("ans3")
 function promiseChain(alpha, timeout){
@@ -74,6 +82,10 @@ function visible(){
 visible();
 
 
+
+
+
+
 // Question 4
 let ans4 = document.getElementById("ans4")
 let variable = true 
@@ -98,6 +110,9 @@ promiseFun(variable).then((data)=>{
 
 
 
+
+
+
 // Question 5
 let ans5 = document.getElementById("ans5")
 function divide(num1, num2, callback) {
@@ -113,7 +128,71 @@ function divide(num1, num2, callback) {
 divide(100, 100,  division);
 
 
+
+
+
+
 // Question 6
+let ans6 = document.getElementById("ans6")
+
+setTimeout(()=>{
+    console.log("1")
+    ans6.innerHTML = "1 <br>"
+    setTimeout(()=>{
+        console.log("2")
+        ans6.innerHTML = ans6.innerHTML+ "2 <br>"
+        setTimeout(()=>{
+            console.log("3");
+            ans6.innerHTML = ans6.innerHTML + "3 <br>"
+            setTimeout(()=>{
+                console.log("4");
+                ans6.innerHTML = ans6.innerHTML + "4 <br>"
+                setTimeout(()=>{
+                    console.log("5");
+                    ans6.innerHTML = ans6.innerHTML + "5 <br>"
+                    setTimeout(()=>{
+                        console.log("6");
+                        ans6.innerHTML = ans6.innerHTML+ "6 <br>"
+                        setTimeout(()=>{
+                            console.log("7")
+                            ans6.innerHTML = ans6.innerHTML + "7 <br>"
+                        },7000)
+                    },6000)
+                },5000)
+            },4000)
+        },3000)
+    },2000)
+},1000)
+
+
+
+
+
+// Question 7
+let ans7 = document.getElementById("ans7")
+function findSum(num1,num2){
+    let pr = new Promise(function(res,rej){
+        setTimeout(function(){
+            let sum = num1 + num2;
+            if(isNaN(sum))
+            {
+                rej("Try Again! Enter a valid number");
+            }
+            else
+            {
+                res(sum);
+            }
+        }, 2000)
+    })
+    return pr;
+}
+
+findSum(26,23).then(function(ans){
+    console.log("Sum =", ans);
+    ans7.innerHTML = "Sum = " + ans
+}).catch(function(err){
+    console.log(err);
+})
 
 
 
@@ -121,61 +200,55 @@ divide(100, 100,  division);
 
 
 
+// question 8
+let ans8 = document.getElementById("ans8")
+function fetchData() {
+    return new Promise(resolve => {
+      setTimeout(() => resolve('Data received!'), 2000);
+    });
+  }
+  
+  async function printData() {
+    console.log("Fetching data...");
+    ans8.innerHTML = "Fetching data....." + "<br>"
+    const data = await fetchData();
+    console.log(data);
+    ans8.innerHTML = ans8.innerHTML + data
+  }
+  
+printData();
 
 
 
-
+// Question 9
+let ans9 = document.getElementById("ans9")
 function mypromise(num ){
     return new Promise((resolve, reject)=>{
         if(num%2 === 0){
-            resolve("This is even")
+            resolve("This is even ")
         }
         else{
-            rej("This is Odd")
+            rej("This is Odd ")
         }
     })
 }
 
-
 const mypromise1 = new Promise((resolve, reject)=>{
     if(true){
-        resolve("hi EAC _01")
+        resolve(" hi EAC _01 ")
     }
     else{
-        reject("Soryy !!!!")
+        reject(" Soryy !!!! ")
     }
 })
 
 Promise.all([mypromise(20), mypromise1]).then((data)=>{
     console.log(data)
+    ans9.innerHTML = data
 }).catch((data)=>{
     console.log(data)
+    ans9.innerHTML = data
 })
-
-
-
-
-
-
-
-
-
-function display(alpha, timeout){
-    setTimeout(()=>{
-        console.log(alpha);
-    },timeout);
-}
-
-
-function visible1(){
-    display("A", 2000);
-    display("E", 4000);
-    display("I", 1000);
-    display("O", 2000);
-    display("U", 5000);
-}
-
-visible1();
 
 
 
